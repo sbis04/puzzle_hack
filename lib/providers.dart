6389c9d7ac131f8466b87783e4d58ea66ed3648c
Souvik Biswas <sbis1999@gmail.com>
2022-01-30 16:05:58 +0530

    Add providers

---
+++ lib/providers.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_flutter_puzzle/utils/database_client.dart';
+
+import 'application/notifiers/anonymous_auth_notifier.dart';
+import 'application/states/anonymous_auth_state.dart';
+import 'utils/authentication_client.dart';
+
+final authenticationClientProvider = Provider<AuthenticationClient>(
+  (ref) => AuthenticationClient(),
+);
+
+final databaseClientProvider = Provider<DatabaseClient>(
+  (ref) => DatabaseClient(),
+);
+
+final anonymousAuthNotificationProvider = StateNotifierProvider.autoDispose<
+    AnonymousAuthNotifier, AnonymousAuthState>(
+  (ref) => AnonymousAuthNotifier(
+    ref.watch(authenticationClientProvider),
+    ref.watch(databaseClientProvider),
+  ),
+);
