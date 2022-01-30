bbebbc473fab193fc67bbdc37b399b93518779c2
Souvik Biswas <sbis1999@gmail.com>
2022-01-30 16:05:30 +0530

    Add database notifier - not used currently

---
+++ lib/application/notifiers/add_user_database_notifier.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_flutter_puzzle/application/states/add_user_database_state.dart';
+import 'package:my_flutter_puzzle/models/user_info.dart';
+import 'package:my_flutter_puzzle/utils/database_client.dart';
+
+class AddUserDatabaseNotifier extends StateNotifier<AddUserDatabaseState> {
+  final DatabaseClient _databaseClient;
+
+  AddUserDatabaseNotifier(this._databaseClient)
+      : super(const AddUserDatabaseState());
+
+  triggerAddUser({required UserData user}) async {
+    state = const AddUserDatabaseState.processing();
+
+    try {
+      await _databaseClient.addUser(userInfo: user);
+      state = AddUserDatabaseState.done(user);
+    } catch (e) {
+      state = AddUserDatabaseState.error(message: e.toString());
+    }
+  }
+}
