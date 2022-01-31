68cbf2b71476a292080fb7e649404e51eef5eaac
Souvik Biswas <sbis1999@gmail.com>
2022-01-31 11:25:12 +0530

    Add new Strings

--- lib/res/strings.dart
+++ lib/res/strings.dart
@@ -1,5 +1,12 @@
 class Strings {
   static const usersCollectionName = 'users';
+  static const queueCollectionName = 'queue';
+  static const matchedCollectionName = 'matched';
+  static const idFieldName = 'id';
+  static const myuidFieldName = 'myuid';
+  static const otheruidFieldName = 'otheruid';
+  static const mylistFieldName = 'mylist';
+  static const otherlistFieldName = 'otherlist';
 }
 
 /// username can be made like this
