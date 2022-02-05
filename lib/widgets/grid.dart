b0de9ecf0cc6b0fe97d1a6ed5540aed71598edee
Souvik Biswas <sbis1999@gmail.com>
2022-02-05 10:37:22 +0530

    Remove tuple

--- lib/widgets/grid.dart
+++ lib/widgets/grid.dart
@@ -28,51 +28,37 @@ class Grid extends StatelessWidget {
         itemCount: number.length,
         itemBuilder: (context, index) {
           return number[index] != 0
-              ? Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: GestureDetector(
-                    onTap: () => onTap(index),
-                    child: AnimatedPositioned(
-                      duration: Duration(milliseconds: 600),
-                      child: Container(
-                        height: 20,
-                        width: 20,
-                        color: Colors.orange,
+              ? MouseRegion(
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: GestureDetector(
+                      onTap: () => onTap(index),
+                      child: Card(
+                        elevation: 4,
+                        color: color,
+                        // dark ->  0xFF14407a
+                        // light ->  0xFF43b9fd
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: Center(
+                            child: Text(
+                              number[index].toString(),
+                              style: const TextStyle(
+                                fontSize: 60,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 )
-              // ? MouseRegion(
-              //     child: Padding(
-              //       padding: const EdgeInsets.all(2.0),
-              //       child: GestureDetector(
-              //         onTap: () => onTap(index),
-              //         child: Card(
-              //           elevation: 4,
-              //           color: color,
-              //           // dark ->  0xFF14407a
-              //           // light ->  0xFF43b9fd
-              //           shape: RoundedRectangleBorder(
-              //             borderRadius: BorderRadius.circular(20),
-              //           ),
-              //           child: SizedBox(
-              //             height: 20,
-              //             width: 20,
-              //             child: Center(
-              //               child: Text(
-              //                 number[index].toString(),
-              //                 style: const TextStyle(
-              //                   fontSize: 60,
-              //                   fontWeight: FontWeight.bold,
-              //                   color: Colors.white,
-              //                 ),
-              //               ),
-              //             ),
-              //           ),
-              //         ),
-              //       ),
-              //     ),
-              //   )
               : const SizedBox();
         },
       ),
