f9ff5bc87e6434d9fd6aeb2ff66f6d488973db0b
Souvik Biswas <sbis1999@gmail.com>
2022-02-05 10:13:24 +0530

    Update solver test

--- test/puzzle_solver_test.dart
+++ test/puzzle_solver_test.dart
@@ -4,8 +4,9 @@ import 'package:my_flutter_puzzle/utils/puzzle_solver.dart';
 void main() {
   test('solver logic test', () {
     final solver = PuzzleSolverClient(size: 4);
+    final board = solver.createRandomBoard();
 
-    final boardStates = solver.runner();
+    final boardStates = solver.runner(board);
     if (boardStates != null) {
       for (var board in boardStates) {
         print(board);
