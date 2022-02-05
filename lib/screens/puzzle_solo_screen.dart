2a16668d502220fd55cc7fc460c338982af60b63
Souvik Biswas <sbis1999@gmail.com>
2022-02-05 19:56:16 +0530

    Add animations

--- lib/screens/puzzle_solo_screen.dart
+++ lib/screens/puzzle_solo_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:my_flutter_puzzle/res/palette.dart';
 import 'package:my_flutter_puzzle/utils/puzzle_solver.dart';
@@ -21,16 +22,28 @@ class _PuzzleSoloScreenState extends State<PuzzleSoloScreen> {
   List<List<int>>? _board2D;
   List<int>? myList;
   int _moves = 0;
-  final _puzzleSize = 3;
+  final _puzzleSize = 4;
   final int _animationSpeedInMilliseconds = 300;
+  bool _isComputing = false;
+  bool _isAutoSolving = false;
+  bool _isSolved = false;
 
   Map<int, FractionalOffset>? _offsetMap;
+  List<int> _solvedList = [];
 
   @override
   void initState() {
     super.initState();
     _solverClient = PuzzleSolverClient(size: _puzzleSize);
     initBoard();
+    generateSolvedList();
+  }
+
+  generateSolvedList() {
+    for (int i = 1; i < _puzzleSize * _puzzleSize; i++) {
+      _solvedList.add(i);
+    }
+    _solvedList.add(0);
   }
 
   scrambleBoard() {
@@ -41,6 +54,7 @@ class _PuzzleSoloScreenState extends State<PuzzleSoloScreen> {
       _board2D = generated2DBoard;
       myList = generated1DBoard;
       _moves = 0;
+      _isSolved = false;
     });
   }
 
@@ -59,7 +73,17 @@ class _PuzzleSoloScreenState extends State<PuzzleSoloScreen> {
 
   startAutoSolver() async {
     if (_board2D != null) {
-      final boardStates = _solverClient.runner(_board2D!);
+      setState(() {
+        _isComputing = true;
+      });
+
+      List<List<int>>? boardStates =
+          await compute(_solverClient.runner, _board2D!);
+
+      setState(() {
+        _isComputing = false;
+        _isAutoSolving = true;
+      });
 
       if (boardStates != null) {
         for (var board in boardStates) {
@@ -74,6 +98,25 @@ class _PuzzleSoloScreenState extends State<PuzzleSoloScreen> {
         }
       }
     }
+    setState(() {
+      _isAutoSolving = false;
+      _isSolved = true;
+    });
+    showCompleteDialogBox(context);
+  }
+
+  isSolved(List<int> currentBoard) {
+    if (listEquals(currentBoard, _solvedList)) {
+      setState(() {
+        _isSolved = true;
+      });
+      return true;
+    }
+    setState(() {
+      _isSolved = false;
+    });
+
+    return false;
   }
 
   onClick(index) {
@@ -206,6 +249,10 @@ class _PuzzleSoloScreenState extends State<PuzzleSoloScreen> {
       updateOffset(myList!);
       setState(() {});
 
+      if (isSolved(myList!)) {
+        showCompleteDialogBox(context);
+      }
+
       log('List: $myList');
       log('-----------------------');
     }
@@ -255,10 +302,51 @@ class _PuzzleSoloScreenState extends State<PuzzleSoloScreen> {
     log('OFFSET MAP: $_offsetMap');
   }
 
+  showCompleteDialogBox(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => Dialog(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Solved successfully!',
+                style: TextStyle(fontSize: 22),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Palette.violet,
+                  onSurface: Palette.violet,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    'OK',
+                    style: TextStyle(fontSize: 22),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
-    var boardSize = screenSize.width * 0.4;
+
+    var boardSize = screenSize.height * 0.6;
 
     var spacing = 4;
     var eachBoxSize = (boardSize / _puzzleSize) - (spacing * (_puzzleSize - 1));
@@ -266,123 +354,148 @@ class _PuzzleSoloScreenState extends State<PuzzleSoloScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: myList != null && _offsetMap != null
-          ? Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Row(),
-                  MovesText(moves: _moves),
-                  SizedBox(
-                    height: boardSize,
-                    width: boardSize,
-                    child: Stack(
-                      children: [
-                        for (int i = 0; i < _offsetMap!.length; i++)
-                          _offsetMap!.entries.toList()[i].key != 0
-                              ? AnimatedAlign(
-                                  alignment:
-                                      _offsetMap!.entries.toList()[i].value,
-                                  duration: Duration(
-                                    milliseconds: _animationSpeedInMilliseconds,
-                                  ),
-                                  curve: Curves.easeInOut,
-                                  child: GestureDetector(
-                                    onTap: () => onClick(myList!.indexOf(
-                                        _offsetMap!.entries.toList()[i].key)),
-                                    child: Card(
-                                      elevation: 4,
-                                      color: const Color(0xFF2868d7),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(20),
-                                      ),
-                                      child: SizedBox(
-                                        height: eachBoxSize,
-                                        width: eachBoxSize,
-                                        child: Center(
-                                          child: Text(
-                                            _offsetMap!.entries
-                                                .toList()[i]
-                                                .key
-                                                .toString(),
-                                            style: const TextStyle(
-                                              fontSize: 60,
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.white,
-                                            ),
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(),
+                MovesText(moves: _moves),
+                SizedBox(
+                  height: boardSize,
+                  width: boardSize,
+                  child: Stack(
+                    children: [
+                      for (int i = 0; i < _offsetMap!.length; i++)
+                        _offsetMap!.entries.toList()[i].key != 0
+                            ? AnimatedAlign(
+                                alignment:
+                                    _offsetMap!.entries.toList()[i].value,
+                                duration: Duration(
+                                  milliseconds: _animationSpeedInMilliseconds,
+                                ),
+                                curve: Curves.easeInOut,
+                                child: GestureDetector(
+                                  onTap: () => onClick(myList!.indexOf(
+                                      _offsetMap!.entries.toList()[i].key)),
+                                  child: Card(
+                                    elevation: 4,
+                                    color: const Color(0xFF2868d7),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: SizedBox(
+                                      height: eachBoxSize,
+                                      width: eachBoxSize,
+                                      child: Center(
+                                        child: Text(
+                                          _offsetMap!.entries
+                                              .toList()[i]
+                                              .key
+                                              .toString(),
+                                          style: const TextStyle(
+                                            fontSize: 60,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
                                           ),
                                         ),
                                       ),
                                     ),
                                   ),
-                                )
-                              : const SizedBox(),
-                      ],
-                    ),
-                  ),
-                  // AnimatedGrid(
-                  //   puzzleSize: _puzzleSize,
-                  //   key: UniqueKey(),
-                  //   number: myList!,
-                  //   offsetList: _offsetList!,
-                  //   onTap: onClick,
-                  //   color: const Color(0xFF2868d7),
-                  // ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 30.0),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.2,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              primary: Palette.violet,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                            ),
-                            onPressed: () {
-                              startAutoSolver();
-                            },
-                            child: const Padding(
-                              padding: EdgeInsets.all(16.0),
-                              child: Text(
-                                'Start Auto Solver',
-                                style: TextStyle(fontSize: 22),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 16.0),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 30.0),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.2,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              primary: Palette.crimson,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                            ),
-                            onPressed: () {
-                              scrambleBoard();
-                            },
-                            child: const Padding(
-                              padding: EdgeInsets.all(16.0),
-                              child: Text(
-                                'Scramble',
-                                style: TextStyle(fontSize: 22),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
+                                ),
+                              )
+                            : const SizedBox(),
                     ],
                   ),
-                ],
-              ),
+                ),
+                // AnimatedGrid(
+                //   puzzleSize: _puzzleSize,
+                //   key: UniqueKey(),
+                //   number: myList!,
+                //   offsetList: _offsetList!,
+                //   onTap: onClick,
+                //   color: const Color(0xFF2868d7),
+                // ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 30.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.2,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Palette.violet,
+                            onSurface: Palette.violet,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                          onPressed: _isComputing || _isAutoSolving || _isSolved
+                              ? null
+                              : () {
+                                  startAutoSolver();
+                                },
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: _isComputing || _isAutoSolving
+                                ? Row(
+                                    children: [
+                                      const SizedBox(
+                                        width: 25,
+                                        height: 25,
+                                        child: CircularProgressIndicator(
+                                          color: Palette.violet,
+                                          strokeWidth: 2,
+                                        ),
+                                      ),
+                                      const SizedBox(width: 16),
+                                      Text(
+                                        _isComputing
+                                            ? 'Computing ...'
+                                            : 'Solving ...',
+                                        style: const TextStyle(fontSize: 20),
+                                      ),
+                                    ],
+                                  )
+                                : const Text(
+                                    'Start Auto Solver',
+                                    style: TextStyle(fontSize: 22),
+                                  ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 16.0),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 30.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.2,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Palette.crimson,
+                            onSurface: Palette.crimson,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                          ),
+                          onPressed: _isComputing || _isAutoSolving
+                              ? null
+                              : () {
+                                  scrambleBoard();
+                                },
+                          child: const Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Text(
+                              'Scramble',
+                              style: TextStyle(fontSize: 22),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             )
           : const SizedBox(),
     );
