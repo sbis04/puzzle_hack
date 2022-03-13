5676106ba0fde5f456a05e526d68cb2cb7dbead6
Souvik Biswas <sbis1999@gmail.com>
2022-03-13 20:01:27 +0530

    Update main -- working photo large

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_flutter_puzzle/res/palette.dart';
+import 'package:my_flutter_puzzle/screens/puzzle/puzzle_starter_screen.dart';
 import 'package:my_flutter_puzzle/screens/solo/solo_screen.dart';
+import 'package:my_flutter_puzzle/utils/color_brightness.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 import 'firebase_options.dart';
@@ -29,13 +32,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Puzzle',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'GoogleSans',
+        backgroundColor: Palette.blue.darken(0.3),
       ),
       // home: const LoginScreen(),
       // home: const TimerTestScreen(),
-      home: const SoloScreen(),
+      home: const PuzzleStarterScreen(),
     );
   }
 }
